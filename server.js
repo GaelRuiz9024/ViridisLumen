93a90dcbf9642601fbbea4a0a532fa6e08283f92
Angel <A01425478@tec.mx>
2024-11-24 08:46:43 -0600

    Conexion mqtt agregada

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
-
-
 const express= require('express')
+const app = express();
+const mqtt = require('mqtt')
+const cors = require('cors')
 require('dotenv').config()
+const port = process.env.PORT;
 
+// Este es de ejemplo
+const resumen = require('./src/routes/exampleRoutes')
 
+/*
+const user = require()
+const locations = require()
+const sensorReadings = require()
+const devices = require()
+const lightSettings = require()
+*/
 
-const app=express();
-const port=process.env.PORT;
-const resumen=require('./src/routes/exampleRoutes')
-
-// Middleware para servir archivos estáticos
-app.use(express.static('public')); // Crea una carpeta 'public' para tu HTML
+app.use(cors)
+app.use(express.static('public'));  // Checar esta linea
 app.use(express.json())
 app.use('/',resumen)
 
+const mqttClient = mqtt.connect(`ws://${process.env.MQTTHOST}`, {
+    clientId: 'nodejs_mqtt_client'
+});
+
+//Suscripcion a un topico
+const topic = 'AngelPrueba'
+
+mqttClient.on('connect', () => {
+    console.log(`Conectado al broker MQTT.`);
+    mqttClient.subscribe(topic, (err) => {
+        if (err) {
+            console.error(`Error al suscribirse al tópico: ${err.message}`);
+        } else {
+            console.log(`Suscrito al tópico: ${topic}`);
+        }
+    });
+});
 
 app.listen(port, ()=>{
-    console.log("servidor corriendo en el puerto"+port)
+    console.log("servidor corriendo en el puerto" + port)
 })
